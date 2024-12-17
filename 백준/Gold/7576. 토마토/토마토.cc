9ecb51e7a8428d4e4f02340d26e88cc836df0e55
SangMin LEE <tkdalsdl1013@naver.com>
2024-12-17 21:37:27 +0900

    [Gold V] Title: 토마토, Time: 280 ms, Memory: 10100 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int arr[1001][1001];
+int Move[2][4] = { {1, -1, 0, 0}, {0, 0, 1, -1} };
+queue<pair<int, int>> g;
+int M, N;
+
+void bfs() { 
+    while (!g.empty()) {
+        int current_x = g.front().first;
+        int current_y = g.front().second;
+        g.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int next_x = current_x + Move[0][i];
+            int next_y = current_y + Move[1][i];
+
+            if ((next_x >= 0 && next_y >= 0) && (next_x < N && next_y < M)) { 
+                if (arr[next_x][next_y] == 0) {  
+                    arr[next_x][next_y] = arr[current_x][current_y] + 1;
+                    g.push(make_pair(next_x, next_y));	
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> M >> N;
+    
+    for (int i = 0; i < N; i++) { 
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 1) 
+                g.push(make_pair(i, j));
+        }
+    }
+
+    bfs(); 
+
+    int result = 0;
+    for (int i = 0; i < N; i++) { 
+        for (int j = 0; j < M; j++) {
+            if (arr[i][j] == 0) {  
+                cout << "-1\n";
+                return 0;
+            }
+            result = max(result, arr[i][j]); 
+        }
+    }
+
+    cout << result - 1 << "\n"; 
+    return 0;
+}
