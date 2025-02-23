6be46e49902850379eb83eb92b2159d0fd989067
SangMin LEE <tkdalsdl1013@naver.com>
2025-02-23 22:44:10 +0900

    [Gold V] Title: 토마토, Time: 220 ms, Memory: 10100 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,63 +1,57 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 using namespace std;
-queue <pair<int,int>> q;
-int M,N;
-int g[1000][1000];
-int K = 0; //익지 않은 토마토의 갯수
-int dx[4] = {1,-1,0,0};
-int dy[4] = {0,0,1,-1};
-void BFS(){
-	while(!q.empty()){
-		int x = q.front().second;
-		int y = q.front().first;
-		q.pop();
-		for(int i = 0; i < 4; i++){
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		//익지 않은 토마토가 있다면
-		if ((nx >=0 && ny >= 0) && (nx < M && ny < N)) {
-			if(g[ny][nx]==0) {
-			g[ny][nx] = g[y][x] + 1;
-			q.push({ny,nx});
-			K--;
-			}
-		}//if문
-		//더 이상 익은 토마토가 없으면
-	
-		}//for문
-	
-	}//while 문
-}//void 함수
-int main(){
-	cin >> M >> N;
-	K = M*N;
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < M; j++){
-			//g[i][j] = 0; //초기화
-			cin>>g[i][j];
-			if (g[i][j] == -1)K--;
-			else if (g[i][j] == 1){ K--; q.push({i,j});} //1(시작점 좌표)저장
-		}//2중
-	}//1중
-	//생각 못함, 익은 토마토가 없을 때
-	if (K == 0){cout << 0; return 0;}
-	/*cout << "\n";
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			cout <<i<<"," << j<<":"<< g[i][j] << "\n";
-		}//2중
-	}//1중
-	*/
-	BFS();//while문
-	int result = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (g[i][j] == 0){cout<< -1; return 0;} //익지 못한 토마토가 있다면
-			if (g[i][j] > result)result=g[i][j];
-		}//2중
-	}//1중
-	cout << result-1;
-	return 0;
-}//main 함수
+
+int arr[1001][1001];
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
+queue<pair<int, int>> g;
+int M, N;
+
+void bfs() { 
+    while (!g.empty()) {
+        int current_x = g.front().first;
+        int current_y = g.front().second;
+        g.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int next_x = current_x + dx[i];
+            int next_y = current_y + dy[i];
+
+            if ((next_x >= 0 && next_y >= 0) && (next_x < N && next_y < M)) { 
+                if (arr[next_x][next_y] == 0) {  
+                    arr[next_x][next_y] = arr[current_x][current_y] + 1;
+                    g.push(make_pair(next_x, next_y));	
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> M >> N;
+    
+    for (int i = 0; i < N; i++) { 
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 1) 
+                g.push(make_pair(i, j));
+        }
+    }
+
+    bfs(); 
+
+    int result = 0;
+    for (int i = 0; i < N; i++) { 
+        for (int j = 0; j < M; j++) {
+            if (arr[i][j] == 0) {  
+                cout << "-1\n";
+                return 0;
+            }
+            result = max(result, arr[i][j]); 
+        }
+    }
+
+    cout << result - 1 << "\n"; 
+    return 0;
+}
