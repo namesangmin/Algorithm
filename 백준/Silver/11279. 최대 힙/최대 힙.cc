118ab1896a8cc6383592d8fc5d75b9169c6c5124
SangMin LEE <tkdalsdl1013@naver.com>
2025-12-31 16:46:15 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	priority_queue<int> q;
+	int n;
+	int input;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		if (input == 0) {
+			if (q.empty()) {
+				cout << "0" << "\n";
+			}
+			else {
+				cout  << q.top() << "\n";
+				q.pop();
+			}
+		}
+		else {
+			q.push(input);
+		}
+	}
+
+
+	return 0;
+}
