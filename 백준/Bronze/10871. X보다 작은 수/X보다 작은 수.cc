0006fa461d359fdc1f68fd677c1237917cfc6293
SangMin LEE <tkdalsdl1013@naver.com>
2025-12-31 15:19:44 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int N, X, temp; 
+	cin >> N >> X;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		if (temp < X) cout << temp << " ";
+	}
+	return 0;
+}
