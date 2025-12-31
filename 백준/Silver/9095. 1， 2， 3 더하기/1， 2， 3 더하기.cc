52e923dd482271f630543ac60952ffcb301ac4d3
SangMin LEE <tkdalsdl1013@naver.com>
2025-12-31 16:44:55 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+// 1,2,3 합으로 나타내기
+// n은 11보다 작음 1~10
+int dp[12];
+int f(int n) {
+	// 이미 계산한 값이면 return;
+	if (dp[n] > 0 ) { return dp[n]; }
+
+	if (n > 0) {
+		// n이 양수이면 계속 함수를 돈다.
+		// 여기서 n 값을 조정하고 재귀함수
+		return dp[n] = f(n-1)+f(n-2)+f(n-3);
+	}
+	else if (n < 0) {
+		// n이 음수이면 스탑(false), return 0
+		return false;
+	}
+	else if(n==0){
+		// n=0이면 나눠지는 것이므로 true, return 1
+		return true;
+	}
+}
+int main() {
+	int n;
+	int input;
+	int result;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		result = f(input);
+		cout << result << "\n";
+	}
+	return 0;
+}
