34fa6b9220b21687ce5975536bd11220e71024a1
SangMin LEE <tkdalsdl1013@naver.com>
2026-01-07 10:17:44 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -5,7 +5,6 @@ using namespace std;
 
 
 int main() {
-	vector<char> v;
 	vector<int> num(10, 0);
 	char tmCh;
 	while (true) {
@@ -16,11 +15,8 @@ int main() {
 	}
 	num[6] += num[9];
 	num[9] = 0;
-	if (num[6] >= 2) {
-		if (num[6] % 2 == 1)num[6]++;
-		num[6] /= 2;
-	}
-	sort(num.begin(), num.end());
-	cout << num[9] << "\n";
+    num[6] = (num[6] + 1) / 2;
+    int maxVal = *max_element(num.begin(), num.end());
+    cout << maxVal << "\n";
 	return 0;
 }
