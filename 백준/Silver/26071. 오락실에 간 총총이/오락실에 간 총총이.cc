70c3e25cef6676d5d943b11e6fcd589ea1dbbbc8
SangMin LEE <tkdalsdl1013@naver.com>
2026-03-03 09:24:57 +0900

    [Silver II] Title: 오락실에 간 총총이, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/26071. 오락실에 간 총총이/오락실에 간 총총이.cc
+++ 백준/Silver/26071. 오락실에 간 총총이/오락실에 간 총총이.cc
@@ -2,34 +2,24 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-int N;
+int N, ans;
 vector<pair<int, int>> gom;
 void input() {
     cin >> N;
     char tm;
+    int L = 0, R = 110101010, U = 0, D = 101010101;
+    int x = 0, y = 0;
 
     for (int r = 0; r < N; r++) {
         for (int c = 0; c < N; c++) {
             cin >> tm;
             if (tm == 'G') {
-                gom.push_back({ r,c });
+                U = max(U, r);
+                D = min(D, r);
+                L = max(L, c);
+                R = min(R, c);
             }
         }
-    }
-}
-int func() {
-    int ans = 0;
-    int L = 0, R = 110101010, U = 0, D = 101010101;
-    int x = 0, y = 0;
-
-    for (int i = 0; i < gom.size(); i++) {
-        int cx = gom[i].second;
-        int cy = gom[i].first;
-        
-        U = max(U, cy);
-        D = min(D, cy);
-        L = max(L, cx);
-        R = min(R, cx);
     }
     if (L == R && U == D) {
         x = 0; y = 0;
@@ -37,7 +27,7 @@ int func() {
     else if (U == D) {
         x = min(L, (N - 1) - R);
     }
-    else if(L==R){
+    else if (L == R) {
         y = min(U, (N - 1) - D);
     }
     else {
@@ -45,13 +35,12 @@ int func() {
         y = min(U, (N - 1) - D);
     }
     ans = x + y;
-
-    return ans;
 }
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     input();
-    cout << func() << "\n";
+    cout <<ans<< "\n";
     return 0;
 }
