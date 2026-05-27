7f1880e0fede2c9482ff444c5367d70915fff08a
SangMin LEE <tkdalsdl1013@naver.com>
2026-05-28 01:27:32 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.91 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
@@ -4,51 +4,52 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-
 map<string, int> m;
-void find_str(string& order, string current, int index, int len){
-    if(len == current.size()){
-        m[current]++;
+void find_set(string order, string tm_memo, int idx, int c_course){
+    if(c_course == tm_memo.size()){
+        m[tm_memo]++;   
         return;
     }
     
-    for(int i = index; i< order.size(); i++){
-        find_str(order, current + order[i], i + 1, len);
+    for(int i= idx; i< order.size(); i++){
+        find_set(order, tm_memo + order[i], i + 1, c_course);
     }
 }
-
 bool cmp(const string& a, const string& b){
     return a < b;
 }
-
 vector<string> solution(vector<string> orders, vector<int> course) {
     vector<string> answer;
-    for(int i = 0; i < orders.size(); i++) {
+    
+    for(int i=0; i< orders.size(); i++){
         sort(orders[i].begin(), orders[i].end());
     }
     
-    for(int c =0; c < course.size(); c++){
+    // 1. course의 개수는 몇 개 있나? -> map으로 개수를 셈 -> 그 중에서 가장 큰 값을 찾고, 
+    // 2. 조합 중에서 큰 값과 일치하는게 뭔가?
+    for(int idx = 0; idx < course.size(); idx++){
+        int c_course = course[idx];
         m.clear();
-
-       for(int i=0; i< orders.size(); i++){
-           string tm = {};
-           find_str(orders[i], tm, 0, course[c]);
-       }
         
-        int max_len = 0;
-        for(auto a : m){
-            if(a.second > max_len){
-                max_len = a.second;
-            }
+        for(int o =0 ; o< orders.size(); o++){
+            string tm = {};
+            find_set(orders[o], tm, 0, c_course);
         }
         
-        if(max_len >=2){
+            int max_cnt = 0;
             for(auto a : m){
-                if(a.second == max_len){
-                    answer.push_back(a.first);
+                if(max_cnt < a.second){
+                    max_cnt = a.second;
                 }
             }
-        }
+        
+            if(max_cnt >= 2){
+                for(auto a : m){
+                    if(max_cnt <= a.second){
+                        answer.push_back(a.first);
+                    }
+                }                
+            }
     }
     sort(answer.begin(), answer.end(), cmp);
     return answer;
